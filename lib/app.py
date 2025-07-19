9e7f7eff2891c0594626e08ffc48c3dc556fc796
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-20 00:05:24 +0300

    Print Ending

--- lib/app.py
+++ lib/app.py
@@ -1,1 +1,4 @@
-print("Hello world!")
+# Print is a built-in Python function that outputs a string of text to the Terminal
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
