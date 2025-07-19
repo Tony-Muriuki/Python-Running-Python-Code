9e42a914c63b4d4d5a330d59c3ad0902ff93f807
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-20 00:20:09 +0300

    Running Python

--- lib/app.py
+++ lib/app.py
@@ -1,4 +1,2 @@
 # Print is a built-in Python function that outputs a string of text to the Terminal
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
+print("Hello World! Pass this test, please.")
